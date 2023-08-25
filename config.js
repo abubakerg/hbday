3b1358a46a1870a2f7a8e9cc6160a23dd876a49f
M.Abubaker <106866152+abubakerg@users.noreply.github.com>
2023-08-25 23:39:30 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,47 @@
+// Badu, Aotian October 10, 2019 https://github.com/AJLoveChina/birthday
+var config = {
+    // The length of the sentence can be arbitrary, you can write ten sentences, twenty sentences are fine
+    // Try not to exceed 15 characters per sentence, otherwise the display effect may not be very good
+    texts: [
+        //Here, the last comma at the end of each sentence must be in English!! blushyyyVery important!!
+        "Happiest birthday to my number one, my human diary", // ditto...
+        "my sweetest friend <br/> blushyyy",
+        "there's so much I want to say and express",
+        "but i don't know how to write it down here",
+        "Just Thank you for being this person that you are",
+        "i feel grateful to have crossed paths with you",
+        "and grateful to have known you ",
+        "My wish for you is that...",
+        "this life becomes all that you want",
+        "your dreams stay big",
+        "worries stay small",
+        "And while you're out there getting where your getting to...",
+        "I hope you know somebody loves you and wants the same things too",
+        "Yeah this is my wish for you <br/> Enjoy Your day :)"
+    ],
+    /**
+     * imgs can be left blank, but if you want to fill in, you must follow the format below
+     * "Corresponding to the above text, it must be exactly the same" : "The image address, you can put the image in the imgs folder"
+     * For example
+     * "Beloved little cutie": "./imgs/xiaokeai.jpg"
+     *
+     * If you don't want pictures, just write two slash comments at the beginning of each line, for example, the picture "Today is your birthday" below will not be displayed :)
+     * Tip: It is better to use a square or close to a square for the picture, it looks better
+     */
+    imgs: {
+        "Dear little cutie": "./imgs/xiaokeai.png",
+        // "Today is your birthday": "./imgs/birthday.jpg",
+    },
+    // button text description, the following is the default button text, in English, you can change it to your favorite text
+    desc: {
+        turn_on: "Turn on Lights",
+        play: "Play Music",
+        banner_coming: "Lets Decorate",
+        balloons_flying: "Looks like something is missing",
+        cake_fadein: "Cake?",
+        light_candle: "Candle?",
+        wish_message: "Happy Birthday :) <br/> (Click Again)",
+        story: "A MESSAGE FOR YOU",
+        
+    }
+};
